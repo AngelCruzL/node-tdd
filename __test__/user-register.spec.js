656cd6bb952052541fc788b1b4e2f368cbb2c4a1
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2023-07-23 11:45:13 -0600

    Add test cases to test the save user functionality and pass them

--- __test__/user-register.spec.js
+++ __test__/user-register.spec.js
@@ -1,5 +1,12 @@
 const request = require('supertest');
+
 const app = require('../src/app');
+const sequelize = require('../src/config/database');
+const User = require('../src/models/User');
+
+beforeAll(() => sequelize.sync());
+
+beforeEach(() => User.destroy({ truncate: true }));
 
 describe('User register', () => {
   it('should return 200 OK when signup request is valid', done => {
@@ -27,6 +34,43 @@ describe('User register', () => {
       .then(response => {
         expect(response.body.message).toBe('User created successfully');
         done();
+      });
+  });
+
+  it('should save the user on database', done => {
+    request(app)
+      .post('/api/1.0/users')
+      .send({
+        username: 'user1',
+        email: 'user1@mail.com',
+        password: 'Secret123',
+      })
+      .then(() => {
+        User.findAll().then(users => {
+          expect(users.length).toBe(1);
+          done();
+        });
+      });
+  });
+
+  it('should send user data match with inserted user data', done => {
+    const username = 'user1';
+    const email = 'user1@mail.com';
+
+    request(app)
+      .post('/api/1.0/users')
+      .send({
+        username,
+        email,
+        password: 'Secret123',
+      })
+      .then(() => {
+        User.findAll().then(users => {
+          const insertedUser = users[0];
+          expect(insertedUser.username).toBe(username);
+          expect(insertedUser.email).toBe(email);
+          done();
+        });
       });
   });
 });
--- src/app.js
+++ src/app.js
@@ -1,9 +1,15 @@
 const express = require('express');
 
+const User = require('./models/User');
+
 const app = express();
 
+app.use(express.json());
+
 app.post('/api/1.0/users', (req, res) => {
-  return res.send({ message: 'User created successfully' });
+  User.create(req.body).then(() => {
+    return res.send({ message: 'User created successfully' });
+  });
 });
 
 module.exports = app;
