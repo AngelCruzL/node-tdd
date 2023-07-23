3c71bffc43188a7ce1299db26d67c2b34c52586a
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2023-07-23 13:40:19 -0600

    Add username validation test case

--- __test__/user-register.spec.js
+++ __test__/user-register.spec.js
@@ -8,40 +8,42 @@ beforeAll(() => sequelize.sync());
 
 beforeEach(() => User.destroy({ truncate: true }));
 
+const username = 'user1';
+const email = 'user1@mail.com';
+const password = 'Secret123';
+
+const validUser = {
+  username,
+  email,
+  password,
+};
+
+const postUser = (user = validUser) => {
+  return request(app).post('/api/1.0/users').send(user);
+};
+
 describe('User register', () => {
-  const username = 'user1';
-  const email = 'user1@mail.com';
-  const password = 'Secret123';
-
-  const postValidUser = () => {
-    return request(app).post('/api/1.0/users').send({
-      username,
-      email,
-      password,
-    });
-  };
-
   it('should return 200 OK when signup request is valid', async () => {
-    const response = await postValidUser();
+    const response = await postUser();
 
     expect(response.status).toBe(200);
   });
 
   it('should return a success message when the signup request is valid', async () => {
-    const response = await postValidUser();
+    const response = await postUser();
 
     expect(response.body.message).toBe('User created successfully');
   });
 
   it('should save the user on database', async () => {
-    await postValidUser();
+    await postUser();
     const users = await User.findAll();
 
     expect(users.length).toBe(1);
   });
 
   it('should send user data match with inserted user data', async () => {
-    await postValidUser();
+    await postUser();
     const users = await User.findAll();
     const insertedUser = users[0];
 
@@ -50,10 +52,42 @@ describe('User register', () => {
   });
 
   it('should store hashed user password in database', async () => {
-    await postValidUser();
+    await postUser();
     const users = await User.findAll();
     const insertedUser = users[0];
-    
+
     expect(insertedUser.password).not.toBe(password);
+  });
+
+  it('should return 400 when username is null', async () => {
+    const response = await postUser({
+      username: null,
+      email,
+      password,
+    });
+
+    expect(response.status).toBe(400);
+  });
+
+  it('should return the validationErrors field in response body when validation error occurs', async () => {
+    const response = await postUser({
+      username: null,
+      email,
+      password,
+    });
+    const body = response.body;
+
+    expect(body.validationErrors).not.toBeUndefined();
+  });
+
+  it('should return Username cannot be null when username is null', async () => {
+    const response = await postUser({
+      username: null,
+      email,
+      password,
+    });
+    const body = response.body;
+
+    expect(body.validationErrors.username).toBe('Username cannot be null');
   });
 });
--- src/user/user.router.js
+++ src/user/user.router.js
@@ -3,6 +3,15 @@ const router = require('express').Router();
 const UserService = require('./user.service');
 
 router.post('', async (req, res) => {
+  const user = req.body;
+
+  if (user.username === null)
+    return res.status(400).send({
+      validationErrors: {
+        username: 'Username cannot be null',
+      },
+    });
+
   await UserService.save(req.body);
 
   return res.send({ message: 'User created successfully' });
